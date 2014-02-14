8b5af7369b92f3d0a5206048d57ec01675789654
Agbodike <agbodike@gmail.com>
2014-02-13 22:40:53 -0800

    Implemented determination of composite numbers

--- my_prime.rb
+++ my_prime.rb
@@ -1,11 +1,20 @@
 class Integer
   def prime?
     case
+    # simple cases for performance
     when self < 2 then false
     when self == 2 then true
     when self == 3 then true
     when self % 2 == 0 then false
     when self % 3 == 0 then false
+    #general prime test
+    else
+      test_top = Math.sqrt(self).ceil    # get the lowest integer >= to root of the integer in question
+      test_range = (2..test_top)         # Already tested division by 2&3, start with 5 as it is the next prime
+      test_range.each do |test|
+        if (self % test == 0) then return false end
+      end
+      nil
     end
   end
 
