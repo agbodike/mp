1e9c2047b499ea6c19910dced4afc7b4b94abeae
Agbodike <agbodike@gmail.com>
2014-02-13 21:12:43 -0800

    Added spec to test primality for 3

--- spec/my_prime_spec.rb
+++ spec/my_prime_spec.rb
@@ -10,6 +10,11 @@ describe Integer, "#prime?" do
   it "returns true for 2" do
     2.prime?.should eq(true)
   end
+
+  it "returns true for 3" do
+    3.prime?.should eq(true)
+  end
+
   it "returns false for even numbers other than 2" do
     4.prime?.should eq(false)
     128.prime?.should eq(false)
