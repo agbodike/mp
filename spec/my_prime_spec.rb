49537dff43455af059ff44c1e1006c98aa1f1c68
Agbodike <agbodike@gmail.com>
2014-02-13 19:29:09 -0800

    Rearranged specs

--- spec/my_prime_spec.rb
+++ spec/my_prime_spec.rb
@@ -1,17 +1,18 @@
 require '../my_prime.rb'
 
 describe Integer, "#prime?" do
-  it "returns false for non-prime numbers" do
-    4.prime?.should eq(false)
-    9.prime?.should eq(false)
-    14.prime?.should eq(false)
-    225.prime?.should eq(false)
+  it "returns false for less than 2" do
+    1.prime?.should eq(false)
+    0.prime?.should eq(false)
+    -12.prime?.should eq(false)
   end
 
-  it "returns true for prime numbers" do
+  it "returns true for 2" do
     2.prime?.should eq(true)
-    3.prime?.should eq(true)
-    11.prime?.should eq(true)
-    101.prime?.should eq(true)
+  end
+  it "returns false for even numbers other than 2" do
+    4.prime?.should eq(false)
+    128.prime?.should eq(false)
+    101006.prime?.should eq(false)
   end
 end
