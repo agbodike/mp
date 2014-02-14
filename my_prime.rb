64f73ef775c3184a6a03402fe39bb3859c4a18eb
Agbodike <agbodike@gmail.com>
2014-02-13 23:03:10 -0800

    Optimization

--- my_prime.rb
+++ my_prime.rb
@@ -10,7 +10,8 @@ class Integer
     #general prime test
     else
       test_top = Math.sqrt(self).ceil    # get the lowest integer >= to root of the integer in question
-      test_range = (2..test_top)         # Already tested division by 2&3, start with 5 as it is the next prime
+      return true if test_top < 5        # Any composite with a root ceiling less than 5 would have been found above
+      test_range = (5..test_top)         # Already tested division by 2&3, start with 5 as it is the next prime
       test_range.each do |test|
         if (self % test == 0) then return false end
       end
